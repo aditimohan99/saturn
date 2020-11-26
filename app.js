336262df6169956fded8cedf8985eb7a3c91363c
aditimohan99 <74734747+aditimohan99@users.noreply.github.com>
2020-11-26 18:37:37 +0000

    homepage.js

---
+++ app.js
@@ -0,0 +1,40 @@
+//PROMPT //
+
+
+  var goodnews = alert("Are you looking for something positive?");
+  var answer = prompt("Yes [y] or No [n]");
+  if (answer === "y"){
+    alert("You've come to the right place!");
+  } else {
+      alert("Maybe try BBC news instead.")
+  };
+
+
+// SLIDES //
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
